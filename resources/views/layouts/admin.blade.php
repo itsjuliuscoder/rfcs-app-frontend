822b8930a90057682e401b67deee01b5b10bd839
Julius Olajumoke <juliusolajumokecoder@gmail.com>
2021-01-12 08:59:33 +0100

    Changes made as regards the favicon file

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <title>@yield('title', 'Login Page')</title>
-    <link rel="icon" type="image/x-icon" href="favicon.ico"/>
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon"/>
     @yield('stylesheets')
 </head>
 @yield('body')
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -7,7 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon"/>
 	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,400i,500,500i,600,700&display=swap" rel="stylesheet">
 
 	<link rel="stylesheet" href="css/studiare-assets.min.css">
